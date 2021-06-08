ddf51fbd4fcb3367bbaf339b6ffdb0b5e11ac4d9
刘富 <1459200389@qq.com>
2021-06-08 21:22:55 +0800

    完善商品分类参数页面的功能

--- src/views/goods/Params.vue
+++ src/views/goods/Params.vue
@@ -18,18 +18,18 @@
         <el-cascader
           v-model="value"
           :options="options"
-          @change="handleChange"
+          @change="handleCascaderChange"
         ></el-cascader>
       </div>
       <el-tabs v-model="activeName" @tab-click="handleClick">
         <el-tab-pane label="动态参数" name="first">
           <el-button type="primary" size="mini">添加参数</el-button>
-          <el-table :data="tableData" border style="width: 100%">
+          <el-table :data="dynamicParameterData" border style="width: 100%">
             <el-table-column type="expand">
-              <template>
+              <template slot-scope="scope">
                 <el-tag
                   :key="tag"
-                  v-for="tag in dynamicTags"
+                  v-for="tag in scope.row.params_tag"
                   closable
                   :disable-transitions="false"
                   @close="handleClose(tag)"
@@ -51,345 +51,188 @@
                   class="button-new-tag"
                   size="small"
                   @click="showInput"
-                  >+ New Tag</el-button
+                  >+ 添加新参数标签</el-button
                 >
               </template>
             </el-table-column>
             <el-table-column type="index" width="50"> </el-table-column>
-            <el-table-column label="商品 ID" prop="id"> </el-table-column>
-            <el-table-column label="商品名称" prop="name"> </el-table-column>
-            <el-table-column label="描述" prop="desc"> </el-table-column>
+            <el-table-column label="参数名称" prop="params_name">
+            </el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button
+                  type="primary"
+                  icon="el-icon-edit"
+                  size="mini"
+                  @click="showDialog('修改动态参数名称', 'editParams')"
+                  >编辑</el-button
+                >
+                <el-button type="danger" icon="el-icon-delete" size="mini"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
           </el-table>
         </el-tab-pane>
         <el-tab-pane label="静态属性" name="second">
           <el-button type="primary" size="mini">添加属性</el-button>
-          <el-table :data="tableData" border style="width: 100%">
+          <el-table :data="staticPropertiesData" border style="width: 100%">
             <el-table-column type="expand">
-              <template slot-scope="props">
-                <el-form label-position="left" inline class="demo-table-expand">
-                  <el-form-item label="商品名称">
-                    <span>{{ props.row.name }}</span>
-                  </el-form-item>
-                  <el-form-item label="所属店铺">
-                    <span>{{ props.row.shop }}</span>
-                  </el-form-item>
-                  <el-form-item label="商品 ID">
-                    <span>{{ props.row.id }}</span>
-                  </el-form-item>
-                  <el-form-item label="店铺 ID">
-                    <span>{{ props.row.shopId }}</span>
-                  </el-form-item>
-                  <el-form-item label="商品分类">
-                    <span>{{ props.row.category }}</span>
-                  </el-form-item>
-                  <el-form-item label="店铺地址">
-                    <span>{{ props.row.address }}</span>
-                  </el-form-item>
-                  <el-form-item label="商品描述">
-                    <span>{{ props.row.desc }}</span>
-                  </el-form-item>
-                </el-form>
+              <template slot-scope="scope">
+                <el-tag
+                  :key="tag"
+                  v-for="tag in scope.row.attribute_tag"
+                  closable
+                  :disable-transitions="false"
+                  @close="handleClose(tag)"
+                >
+                  {{ tag }}
+                </el-tag>
+                <el-input
+                  class="input-new-tag"
+                  v-if="inputVisible"
+                  v-model="inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm"
+                  @blur="handleInputConfirm"
+                >
+                </el-input>
+                <el-button
+                  v-else
+                  class="button-new-tag"
+                  size="small"
+                  @click="showInput"
+                  >+ 添加新属性标签</el-button
+                >
               </template>
             </el-table-column>
             <el-table-column type="index" width="50"> </el-table-column>
-            <el-table-column label="商品 ID" prop="id"> </el-table-column>
-            <el-table-column label="商品名称" prop="name"> </el-table-column>
-            <el-table-column label="描述" prop="desc"> </el-table-column>
+            <el-table-column label="属性名称" prop="attribute_name">
+            </el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button
+                  type="primary"
+                  icon="el-icon-edit"
+                  size="mini"
+                  @click="showDialog('修改静态属性名称', 'editParams')"
+                  >编辑</el-button
+                >
+                <el-button type="danger" icon="el-icon-delete" size="mini"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
           </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
+    <DiaLogComponent
+      :dialogTitle="dialogTitle"
+      :dialogShow="dialogShow"
+      :dialogContent="dialogContent"
+      @dialog-close="dialogClose($event)"
+    ></DiaLogComponent>
   </div>
 </template>
 
 <script>
+// 引入封装的 dialog 组件
+import DiaLogComponent from '../../components/Dialog'
+
 export default {
   data() {
     return {
       // cascader 级联选择器
       value: [],
-      options: [
-        {
-          value: 'zhinan',
-          label: '指南',
-          children: [
-            {
-              value: 'shejiyuanze',
-              label: '设计原则',
-              children: [
-                {
-                  value: 'yizhi',
-                  label: '一致'
-                },
-                {
-                  value: 'fankui',
-                  label: '反馈'
-                },
-                {
-                  value: 'xiaolv',
-                  label: '效率'
-                },
-                {
-                  value: 'kekong',
-                  label: '可控'
-                }
-              ]
-            },
-            {
-              value: 'daohang',
-              label: '导航',
-              children: [
-                {
-                  value: 'cexiangdaohang',
-                  label: '侧向导航'
-                },
-                {
-                  value: 'dingbudaohang',
-                  label: '顶部导航'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          value: 'zujian',
-          label: '组件',
-          children: [
-            {
-              value: 'basic',
-              label: 'Basic',
-              children: [
-                {
-                  value: 'layout',
-                  label: 'Layout 布局'
-                },
-                {
-                  value: 'color',
-                  label: 'Color 色彩'
-                },
-                {
-                  value: 'typography',
-                  label: 'Typography 字体'
-                },
-                {
-                  value: 'icon',
-                  label: 'Icon 图标'
-                },
-                {
-                  value: 'button',
-                  label: 'Button 按钮'
-                }
-              ]
-            },
-            {
-              value: 'form',
-              label: 'Form',
-              children: [
-                {
-                  value: 'radio',
-                  label: 'Radio 单选框'
-                },
-                {
-                  value: 'checkbox',
-                  label: 'Checkbox 多选框'
-                },
-                {
-                  value: 'input',
-                  label: 'Input 输入框'
-                },
-                {
-                  value: 'input-number',
-                  label: 'InputNumber 计数器'
-                },
-                {
-                  value: 'select',
-                  label: 'Select 选择器'
-                },
-                {
-                  value: 'cascader',
-                  label: 'Cascader 级联选择器'
-                },
-                {
-                  value: 'switch',
-                  label: 'Switch 开关'
-                },
-                {
-                  value: 'slider',
-                  label: 'Slider 滑块'
-                },
-                {
-                  value: 'time-picker',
-                  label: 'TimePicker 时间选择器'
-                },
-                {
-                  value: 'date-picker',
-                  label: 'DatePicker 日期选择器'
-                },
-                {
-                  value: 'datetime-picker',
-                  label: 'DateTimePicker 日期时间选择器'
-                },
-                {
-                  value: 'upload',
-                  label: 'Upload 上传'
-                },
-                {
-                  value: 'rate',
-                  label: 'Rate 评分'
-                },
-                {
-                  value: 'form',
-                  label: 'Form 表单'
-                }
-              ]
-            },
-            {
-              value: 'data',
-              label: 'Data',
-              children: [
-                {
-                  value: 'table',
-                  label: 'Table 表格'
-                },
-                {
-                  value: 'tag',
-                  label: 'Tag 标签'
-                },
-                {
-                  value: 'progress',
-                  label: 'Progress 进度条'
-                },
-                {
-                  value: 'tree',
-                  label: 'Tree 树形控件'
-                },
-                {
-                  value: 'pagination',
-                  label: 'Pagination 分页'
-                },
-                {
-                  value: 'badge',
-                  label: 'Badge 标记'
-                }
-              ]
-            },
-            {
-              value: 'notice',
-              label: 'Notice',
-              children: [
-                {
-                  value: 'alert',
-                  label: 'Alert 警告'
-                },
-                {
-                  value: 'loading',
-                  label: 'Loading 加载'
-                },
-                {
-                  value: 'message',
-                  label: 'Message 消息提示'
-                },
-                {
-                  value: 'message-box',
-                  label: 'MessageBox 弹框'
-                },
-                {
-                  value: 'notification',
-                  label: 'Notification 通知'
-                }
-              ]
-            },
-            {
-              value: 'navigation',
-              label: 'Navigation',
-              children: [
-                {
-                  value: 'menu',
-                  label: 'NavMenu 导航菜单'
-                },
-                {
-                  value: 'tabs',
-                  label: 'Tabs 标签页'
-                },
-                {
-                  value: 'breadcrumb',
-                  label: 'Breadcrumb 面包屑'
-                },
-                {
-                  value: 'dropdown',
-                  label: 'Dropdown 下拉菜单'
-                },
-                {
-                  value: 'steps',
-                  label: 'Steps 步骤条'
-                }
-              ]
-            },
-            {
-              value: 'others',
-              label: 'Others',
-              children: [
-                {
-                  value: 'dialog',
-                  label: 'Dialog 对话框'
-                },
-                {
-                  value: 'tooltip',
-                  label: 'Tooltip 文字提示'
-                },
-                {
-                  value: 'popover',
-                  label: 'Popover 弹出框'
-                },
-                {
-                  value: 'card',
-                  label: 'Card 卡片'
-                },
-                {
-                  value: 'carousel',
-                  label: 'Carousel 走马灯'
-                },
-                {
-                  value: 'collapse',
-                  label: 'Collapse 折叠面板'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          value: 'ziyuan',
-          label: '资源',
-          children: [
-            {
-              value: 'axure',
-              label: 'Axure Components'
-            },
-            {
-              value: 'sketch',
-              label: 'Sketch Templates'
-            },
-            {
-              value: 'jiaohu',
-              label: '组件交互文档'
-            }
-          ]
-        }
-      ],
+      options: [],
       // tabs 标签
       activeName: 'second',
       // table 表格
-      tableData: [],
+      dynamicParameterData: [],
+      staticPropertiesData: [],
       // tag 标签
       dynamicTags: ['标签一', '标签二', '标签三'],
       inputVisible: false,
-      inputValue: ''
+      inputValue: '',
+      // dialog 标签
+      dialogTitle: '',
+      dialogContent: '',
+      dialogShow: false
     }
   },
+  created() {
+    this.getGoodsTag()
+  },
   methods: {
+    async getGoodsTag() {
+      const { data } = await this.$http.get('goods/sort')
+      console.log(data)
+      const goodsSortList = []
+      data.GoodsSortList.map(m => {
+        goodsSortList.push({
+          value: m._id,
+          label: m.goods_sort_name,
+          children: m.children.length === 0 ? [] : this.forGoodsSort(m.children)
+        })
+      })
+      // console.log(goodsSortList)
+      this.options = goodsSortList
+    },
+    showDialog(title, component) {
+      console.log(this)
+      this.dialogShow = true
+      this.dialogTitle = title
+      this.dialogContent = component
+    },
+    dialogClose(val) {
+      console.log(val)
+      this.dialogShow = false
+      if (typeof val === 'string') {
+        this.getGoodsSortList()
+      }
+    },
+    forGoodsSort(goodsChildrenList) {
+      const handlGoodsChildrenList = goodsChildrenList.map(m => {
+        if (m.children && m.children.length !== 0) {
+          // console.log(2333)
+          return {
+            value: m._id,
+            label: m.goods_sort_name,
+            children:
+              m.children.length === 0 ? [] : this.forGoodsSort(m.children)
+          }
+        }
+        return {
+          value: m._id,
+          label: m.goods_sort_name
+        }
+      })
+      return handlGoodsChildrenList
+    },
+    async getSortParamsAttribute(id, pid) {
+      const { data } = await this.$http.get(`goods/tag/${id}`, {
+        params: { pid: pid }
+      })
+      console.log(data.GoodsDetailsTag)
+      const { GoodsDetailsTag } = data
+      // const dynamicParameter =
+      //   data.GoodsDetailsTag[GoodsDetailsTag.length - 1].dynamic_parameter || []
+      // const staticProperties =
+      //   data.GoodsDetailsTag[GoodsDetailsTag.length - 1].static_properties || []
+      this.dynamicParameterData =
+        GoodsDetailsTag[GoodsDetailsTag.length - 1].dynamic_parameter || []
+      this.staticPropertiesData =
+        GoodsDetailsTag[GoodsDetailsTag.length - 1].static_properties || []
+    },
     // cascader 级联选择器
-    handleChange(value) {
+    handleCascaderChange(value) {
       console.log(value)
+      this.getSortParamsAttribute(
+        value[value.length - 1],
+        value[value.length - 2]
+      )
     },
     // tabs 标签
     handleClick(tab, event) {
@@ -414,6 +257,9 @@ export default {
       this.inputVisible = false
       this.inputValue = ''
     }
+  },
+  components: {
+    DiaLogComponent
   }
 }
 </script>
@@ -423,6 +269,16 @@ export default {
   display: flex;
   align-items: center;
   padding: 20px 0;
+}
+
+.el-tag,
+.el-input,
+.el-button {
+  margin-left: 20px;
+}
+
+.el-input {
+  width: 100px;
 }
 
 .el-table {
